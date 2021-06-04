6231a70f0048de7bf4ba5a50b395db399abd5185
scava <sorzesimone1@gmail.com>
2021-06-04 18:37:35 +0200

    implementing conditional

--- main.py
+++ main.py
@@ -1,2 +1,17 @@
 #!/usr/bin/env python
-print("hey")
+import os
+from pathlib import Path
+
+input_path = input("\n👋 Howdy? Enter the path desired: ")
+
+question_path = input("\n👋 Do you want to create folders (press 1) or files (press 2)?: ")
+
+try:
+    val = int(question_path)
+except Exception:
+    print("Woooo, invalid input ❌")
+
+if question_path == 1:
+    print("folder")
+else:
+    print("files")
