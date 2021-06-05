0f608e06e6b5303542b7a9ed5a4a105a2903b2a3
scava <sorzesimone1@gmail.com>
2021-06-05 13:51:29 +0200

    implementation folders creation

--- main.py
+++ main.py
@@ -2,16 +2,38 @@
 import os
 from pathlib import Path
 
-input_path = input("\n👋 Howdy? Enter the path desired: ")
+path = os.getcwd()
+print("The current working directory is %s" % path)
 
-question_path = input("\n👋 Do you want to create folders (press 1) or files (press 2)?: ")
+input_path = path
+
+print(input_path)
+
+question_path = input(
+    "\n👋 Do you want to create folders (press 1) or files (press 2)?: "
+)
 
 try:
     val = int(question_path)
 except Exception:
     print("Woooo, invalid input ❌")
 
-if question_path == 1:
-    print("folder")
+if question_path == str(1):
+    try:
+        folders_names = input(
+            "\n📂 Specify the name of the folders (space for separation): "
+        )
+        folders_to_create = folders_names.split()
+
+        parent_dir = input_path
+
+        for folder in folders_to_create:
+            print(folder)
+            os.mkdir(os.path.join(parent_dir, str(folder)))
+    except OSError:
+        print("Creation of the directory %s failed" % path)
+    else:
+        print("Successfully created the directory %s " % path)
 else:
-    print("files")
+    files_names = input("\n📃 Specify the name of the files (space for separation): ")
+    files_to_create = files_names.split()
